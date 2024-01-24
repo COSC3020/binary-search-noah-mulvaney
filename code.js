6cb1d424fcfafeba77a4b3046e5f1a12be8f6286
Noah Mulvaney <mulvaney.np@gmail.com>
2024-01-24 14:49:07 -0700

    Impliment binarySearch()

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
+//code.js
+//COSC3020 Binary Search Exercise
+//Noah Mulvaney, nmulvane@uywo.edu
+//24 Jan 2024
+
+//Returns index of element in sorted list, -1 if not found
 function binarySearch(list, element) {
-    return -1;
+    var start = 0, end = list.length; //[start, end)
+
+    while (true) {
+        var i = floor((end - start) / 2);
+
+        if (i == 0) return (list[i] == element) ? i : -1;
+
+        if (list[i] > element) {
+            end = i;
+            continue;
+        } else if (list[i] < element) {
+            start = i + 1;
+            continue;
+        } else if (list[i] == element) return i;
+    }
 }
