13e48b4755a9d957289a1c3008597defff4ad242
Noah Mulvaney <mulvaney.np@gmail.com>
2024-01-24 15:39:06 -0700

    Fix binarySearch() (3 try)

--- code.js
+++ code.js
@@ -3,21 +3,32 @@
 //Noah Mulvaney, nmulvane@uywo.edu
 //24 Jan 2024
 
+//Help recieved from lab TA
+
 //Returns index of element in sorted list, -1 if not found
 function binarySearch(list, element) {
     var start = 0, end = list.length; //[start, end)
 
     while (true) {
-        var i = Math.floor((end - start) / 2);
+        //window includes only one element
+        if (start == end) return (list[start] == element) ? start : -1;
 
-        if (i == 0) return (list[start] == element) ? start : -1;
+        //find center value
+        var i = start + Math.floor((end - start) / 2);
 
-        if (list[i] > element) {
-            end = i;
-            continue;
+        if (list[i] > element) { 
+            end = i; //disregard upper half
         } else if (list[i] < element) {
-            start = i + 1;
-            continue;
-        } else if (list[i] == element) return i;
-    }
+            start = i + 1; //disregard lowwer half
+        } else if (list[i] == element) return i; //found
+    } 
 }
+
+/* My own testing
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 1)); //0
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 10)); //10
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 4)); //2
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 5)); //3 or 4
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 6)); //5
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 9)); //9
+*/
