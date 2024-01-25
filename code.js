87364ad4ce0888437a58ec7fe1b6fc5e5f6f3e15
Noah Mulvaney <mulvaney.np@gmail.com>
2024-01-25 12:20:08 -0700

    Fix finding left index on find

--- code.js
+++ code.js
@@ -11,7 +11,13 @@ function binarySearch(list, element) {
 
     while (true) {
         //window includes only one element
-        if (end - start < 2) return (list[start] == element) ? start : -1;
+        if (end - start < 2) {
+            if (list[start] == element) {
+                //decrement start if left value is a duplicate
+                while (list[start - 1] == element) start--;
+                return start; //return lowest index found
+            } else return -1; //element not found
+        }
 
         //find center value
         var i = start + Math.floor((end - start) / 2);
@@ -20,16 +26,19 @@ function binarySearch(list, element) {
             end = i; //disregard upper half
         } else if (list[i] < element) {
             start = i + 1; //disregard lowwer half
-        } else if (list[i] == element) return i; //found
+        } else if (list[i] == element) {
+            //decrement start if left value is a duplicate
+            while (list[i - 1] == element) i--;
+            return i; //return lowest index found
+        }
     } 
 }
 
-/* My own testing
+/* My own testing */
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 1)); //0
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 10)); //10
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 4)); //2
-console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 5)); //3 or 4
+console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 5)); //3
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 6)); //5
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 9)); //9
 console.log(binarySearch([1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10], 3)); //-1
-*/
